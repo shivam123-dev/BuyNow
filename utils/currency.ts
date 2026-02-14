9b22411ab9dbc4fed4da61df881e5f5db7c0b984
RunnerUp <81038095+shivam123-dev@users.noreply.github.com>
2026-02-15 02:44:21 +0530

    Delete utils directory

--- utils/currency.ts
+++
@@ -1,7 +0,0 @@
-export function formatINR(amount: number): string {
-  return new Intl.NumberFormat('en-IN', {
-    style: 'currency',
-    currency: 'INR',
-    maximumFractionDigits: 2,
-  }).format(amount);
-}
